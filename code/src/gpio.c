5c4494a3848d3913336a7a9a70cdcbb809b2f6f3
mleubin <mleubin@087458d4-8f0a-0410-b608-9ee62c3558fa>
2016-09-27 14:22:07 +0000

    New gpio library
    git-svn-id: https://svn.ee.ethz.ch/tecdoc/sigristl/projects/RocketLogger@38379 087458d4-8f0a-0410-b608-9ee62c3558fa

---
+++ code/src/gpio.c
@@ -0,0 +1,212 @@
+#include "gpio.h"
+
+// gpio unexport
+int gpio_unexport(int num) {
+	// open gpio value file
+	int fd = open(GPIO_PATH "unexport", O_WRONLY);
+	if (fd < 0) {
+		printf("Error: could not open GPIO unexport file!\n");
+		return -1;
+	}
+	
+	// export gpio
+	char buf[5] = "";
+	int len = snprintf(buf, sizeof(buf),"%d",num);
+	write(fd, buf, len);
+	
+	// close file
+	close(fd);
+	
+	return 1;
+}
+
+// gpio export
+int gpio_export(int num) {
+	// open gpio value file
+	int fd = open(GPIO_PATH "export", O_WRONLY);
+	if (fd < 0) {
+		printf("Error: could not open GPIO export file!\n");
+		return -1;
+	}
+	
+	// export gpio
+	char buf[5] = "";
+	int len = snprintf(buf, sizeof(buf),"%d",num);
+	write(fd, buf, len);
+	
+	// close file
+	close(fd);
+	
+	return 1;
+}
+
+// set direction
+int gpio_dir(int num, enum direction dir) {
+	
+	// open gpio direction file
+	char pathname[100];
+	sprintf(pathname, GPIO_PATH "gpio%d/direction",num);
+	int fd = open(pathname, O_WRONLY);
+	if (fd < 0) {
+		printf("Error: could not open GPIO direction file!\n");
+		return -1;
+	}
+	
+	// set direction
+	if(dir == OUT) {
+		write(fd, "out", 4);
+	} else {
+		write(fd, "in", 3);
+	}
+	// close file
+	close(fd);
+	
+	return 1;
+}
+
+
+// set interrupt direction
+int gpio_interrupt(int num, enum edge e) {
+	
+	// open gpio edge file
+	char pathname[100];
+	sprintf(pathname, GPIO_PATH "gpio%d/edge",num);
+	int fd = open(pathname, O_WRONLY);
+	if (fd < 0) {
+		printf("Error: could not open GPIO edge file!\n");
+		return -1;
+	}
+	
+	// set edge
+	switch (e) {
+		case NONE:
+			write(fd, "none", 5);
+			break;
+		case RISING:
+			write(fd, "rising", 7);
+			break;
+		case FALLING:
+			write(fd, "falling", 8);
+			break;
+		case BOTH:
+			write(fd, "both", 5);
+			break;
+			
+	}
+	
+	// close file
+	close(fd);
+	
+	return 1;
+}
+
+// set gpio value
+int gpio_set_value(int num, int val) {
+	
+	// open gpio value file
+	char pathname[100];
+	sprintf(pathname, GPIO_PATH "gpio%d/value",num);
+	int fd = open(pathname, O_WRONLY);
+	if (fd < 0) {
+		printf("Error: could not open GPIO value file!\n");
+		return -1;
+	}
+	
+	// set value
+	if(val == 0) {
+		write(fd, "0", 2);
+	} else {
+		write(fd, "1", 2);
+	}
+	
+	// close file
+	close(fd);
+	
+	return 1;
+}
+
+// get gpio value
+int gpio_get_value(int num) {
+	
+	// open gpio value file
+	char pathname[100];
+	sprintf(pathname, GPIO_PATH "gpio%d/value",num);
+	int fd = open(pathname, O_RDONLY);
+	if (fd < 0) {
+		printf("Error: could not open GPIO value file!\n");
+		return -1;
+	}
+	
+	// read value
+	char buf[2] = "";
+	read(fd, &buf, 1);
+	
+	// close file
+	close(fd);
+	
+	return atoi(buf);
+}
+
+// wait on gpio change (returns new value)
+int gpio_wait_interrupt(int num) {
+	
+	// open gpio value file
+	char pathname[100];
+	sprintf(pathname, GPIO_PATH "gpio%d/value",num);
+	int fd = open(pathname, O_RDONLY);
+	if (fd < 0) {
+		printf("Error: could not open GPIO value file!\n");
+		return -1;
+	}
+	
+	// set up polling struct
+	struct pollfd fds;
+	fds.fd = fd;
+	fds.events = POLLPRI;
+	int timeout = -1; // infinite timeout
+	int nfds = 1;
+	int ret;
+	
+	// dummy read (enables blocking polling)
+	char buf[2] = "";
+	read(fds.fd, &buf, 1);
+		
+	// wait on gpio change
+	ret = poll(&fds, nfds, timeout);
+	if (ret < 0) {
+	  printf("Error: poll failed!\n");
+	  return -1;
+	}
+	
+	// read value
+	lseek(fds.fd, 0, SEEK_SET);
+	read(fds.fd, &buf, 1);
+	
+	close(fd);
+	
+	return atoi(buf);
+}
+
+/*int main(int argc, char **argv) {
+	gpio_export(26);
+	gpio_dir(26, IN);
+	gpio_interrupt(26, RISING);
+	int val = gpio_wait_interrupt(26);
+	printf("Interrupt! Value = %d\n",val);
+	gpio_unexport(26);
+	
+	gpio_export(44);
+	gpio_dir(44,OUT);
+	gpio_set_value(44,0);
+	
+	gpio_export(45);
+	gpio_dir(45,OUT);
+	gpio_set_value(45,1);
+	
+	gpio_export(26);
+	gpio_dir(26, IN);
+	int val = gpio_get_value(26);
+	printf("GPIO26 = %d",val);
+	
+	return 1;
+}*/
---
+++ code/src/gpio.h
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <poll.h>
+
+#define GPIO_PATH "/sys/class/gpio/"
+
+enum direction {IN, OUT};
+enum edge {NONE, RISING, FALLING, BOTH};
+
+// gpio unexport
+int gpio_unexport(int num);
+
+// gpio export
+int gpio_export(int num);
+
+// set direction
+int gpio_dir(int num, enum direction dir);
+
+// set interrupt
+int gpio_interrupt(int num, enum edge e);
+
+int gpio_set_value(int num, int val);
+
+int gpio_get_value(int num);
+
+int gpio_wait_interrupt(int num);
