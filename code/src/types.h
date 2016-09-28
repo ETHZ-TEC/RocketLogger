1e9caa161c2085b989143022621c949270c6242a
mleubin <mleubin@087458d4-8f0a-0410-b608-9ee62c3558fa>
2016-09-28 07:59:10 +0000

    Added rl types file
    git-svn-id: https://svn.ee.ethz.ch/tecdoc/sigristl/projects/RocketLogger@38392 087458d4-8f0a-0410-b608-9ee62c3558fa

---
+++ code/src/types.h
@@ -0,0 +1,21 @@
+enum rl_state {OFF, ON, ERROR}
+enum rl_mode {LIMIT, CONTINUOUS, METER};
+enum rl_file_format {NONE, CSV, BIN};
+
+struct rl_conf {
+	enum rl_mode mode;
+	int sample_rate;
+	int update_rate;
+	int number_samples;
+	int channels[10];
+	int force_high_channels[2];
+	int enable_web_server;
+	enum rl_file_format file_format;
+	char file_name[100];
+};
+
+struct rl_state {
+	enum rl_state state;
+	int samples_taken;
+	int buffer_number;
+};
