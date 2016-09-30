ccc0f95b7850a04f4129f9df45343b478c05606e
mleubin <mleubin@087458d4-8f0a-0410-b608-9ee62c3558fa>
2016-09-30 06:58:00 +0000

    Sourced calibration functions out
    git-svn-id: https://svn.ee.ethz.ch/tecdoc/sigristl/projects/RocketLogger@38419 087458d4-8f0a-0410-b608-9ee62c3558fa

---
+++ code/src/calibration.c
@@ -0,0 +1,82 @@
+#include "calibration.h"
+
+// reset calibration
+/**
+ * Set all offsets to default state (0).
+ * @return Indicates success.
+ */
+int reset_offsets() {
+	int i;
+	for (i=0; i< NUM_CALIBRATION_VALUES; i++) {
+		offsets24[i] = 0;
+		offsets16[i] = 0;
+	}
+	return 1;
+}
+
+/**
+ * Set all scales to default state (1).
+ * @return Indicates success.
+ */
+int reset_scales() {
+	int i;
+	for (i=0; i< NUM_CALIBRATION_VALUES; i++) {
+		scales24[i] = 1;
+		scales16[i] = 1;
+	}
+	return 1;
+}
+
+/**
+ * Read in calibration file.
+ * @return Indicates success.
+ */
+int read_calibration() {
+	// check if calibration file existing
+	if(open(CALIBRATION_FILE, O_RDWR) <= 0) {
+		printf("Warning: no calibration file, returning uncalibrated values.\n");
+		reset_offsets();
+		reset_scales();
+		return 1;
+	}
+	// open calibration file
+	FILE* file = fopen(CALIBRATION_FILE, "r");
+	if(file == NULL) {
+		printf("Error: Error opening calibration file");
+		return -1;
+	}
+	// read values
+	fread(offsets24, sizeof(int), NUM_CALIBRATION_VALUES, file);
+	fread(scales24, sizeof(double), NUM_CALIBRATION_VALUES, file);
+	
+	// calculate values for high rates
+	int i;
+	for (i=0; i<NUM_CALIBRATION_VALUES; i++) {
+		offsets16[i] = offsets24[i]/256;
+		scales16[i] = scales24[i]*256;
+	}
+	
+	//close file
+	fclose(file);
+	return 1;
+}
+
+/**
+ * Write calibration values to file.
+ * @return Indicates success.
+ */
+int write_calibration() {
+	// open calibration file
+	FILE* file = fopen(CALIBRATION_FILE, "w+");
+	if(file == NULL) {
+		printf("Error: Error opening calibration file");
+		return -1;
+	}
+	// write values
+	fwrite(offsets24, sizeof(int), NUM_CALIBRATION_VALUES, file);
+	fwrite(scales24, sizeof(double), NUM_CALIBRATION_VALUES, file);
+	
+	//close file
+	fclose(file);
+	return 1;
+}
---
+++ code/src/calibration.h
@@ -0,0 +1,18 @@
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+
+#include "types.h"
+
+#define CALIBRATION_FILE	"/etc/rocketlogger/calibration.dat"
+#define NUM_CALIBRATION_VALUES 10
+
+
+int reset_offsets();
+int reset_scales();
+int read_calibration();
+int write_calibration();
