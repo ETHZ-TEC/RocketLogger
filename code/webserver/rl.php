6ac8c2e45646274de7ecfadb9be2cb7a2f392bba
mleubin <mleubin@student.ethz.ch>
2016-11-04 10:33:04 +0100

    Removed sudo, filtered empty posts

--- code/webserver/rl.php
+++ code/webserver/rl.php
@@ -3,23 +3,25 @@
     if (isset($_POST['command'])) {
 		switch($_POST['command']) {
 			case 'start':
-				$command = 'sudo rocketlogger cont -w -u 1' . $_POST['file'] . $_POST['file_format'] . $_POST['rate'] . $_POST['channels'] . $_POST['force'] . $_POST['digital_inputs'] . ' > /dev/null &';
+				$command = 'rocketlogger cont -w -u 1' . $_POST['file'] . $_POST['file_format'] . $_POST['rate'] . $_POST['channels'] . $_POST['force'] . $_POST['digital_inputs'] . ' > /dev/null &';
 				exec($command);
 				break;
             case 'status':
-			$command = 'rocketloggers ' . $_POST['id'] . ' ' . $_POST['fetchData'] . ' ' . $_POST['timeScale'] . ' ' . $_POST['time'];
-				exec($command, $output);
-				echo json_encode($output);
+				if (isset($_POST['id'])) { // filter empty posts
+					$command = 'rocketloggers ' . $_POST['id'] . ' ' . $_POST['fetchData'] . ' ' . $_POST['timeScale'] . ' ' . $_POST['time'];
+					exec($command, $output);
+					echo json_encode($output);
+				}
 				break;
 			case 'stop':
-				echo exec('sudo rocketlogger stop');
+				echo exec('rocketlogger stop');
 				break;
             case 'get_data':
-				exec('sudo rocketlogger data', $output);
+				exec('rocketlogger data', $output);
 				echo json_encode($output);
 				break;
 			case 'calibrate':
-				exec('sudo rocketlogger calibrate');
+				exec('rocketlogger calibrate');
 				break;
 			case 'delete':
 				$command = 'rm ' . $_POST['filename'];
