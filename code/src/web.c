5b83970f37b134421565131abca5364f4a062fcd
mleubin <mleubin@student.ethz.ch>
2016-11-08 15:53:48 +0100

    Fixed seg fault, pointer in bytes!

--- code/src/web.c
+++ code/src/web.c
@@ -46,7 +46,7 @@ void reset_buffer(struct ringbuffer* buffer, int element_size, int length) {
 }
 
 void buffer_add(struct ringbuffer* buffer, int32_t* data) {
-	memcpy((buffer->data) + buffer->head*buffer->element_size, data, buffer->element_size);
+	memcpy((buffer->data) + buffer->head*buffer->element_size/sizeof(int32_t), data, buffer->element_size);
 	if(buffer->filled < buffer->length) {
 		buffer->filled++;
 	}
@@ -56,5 +56,6 @@ void buffer_add(struct ringbuffer* buffer, int32_t* data) {
 
 int32_t* buffer_get(struct ringbuffer* buffer, int num) {
 	int pos = ((int)buffer->head + (int)buffer->length - 1 - num) % (int)buffer->length;
-	return buffer->data + pos*buffer->element_size;
+	
+	return buffer->data + pos*buffer->element_size/sizeof(int32_t);
 }
