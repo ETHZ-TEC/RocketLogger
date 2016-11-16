b30d6f646b843ee4da9cef8a05734f7dfc387743
Stefan Lippuner <lstefan@student.ethz.ch>
2016-11-16 10:42:33 +0100

    Refactor calculation of ideal values and steps

--- code/matlab/rl_cal.m
+++ code/matlab/rl_cal.m
@@ -1,14 +1,21 @@
-classdef rl_cal
+classdef rl_cal < handle
     %RL_CAL Class that handles the calibration of the RocketLogger
     %   Detailed explanation goes here
     
-    properties(Constant, Hidden)
+    properties(Constant)
         CHANNEL_COUNT = 8;
-        POSITIVE_SCALE_CHANNELS = [1,2,5,6];
+        V_INDEX = [3,4,7,8];
+        IH_INDEX = [1,5];
+        IL_INDEX = [2,6];
+        POSITIVE_SCALE_CHANNELS = [rl_cal.IH_INDEX, rl_cal.IL_INDEX];
         
         FILE_SCALE_V = 1e-6;
         FILE_SCALE_IL = 1e-11;
         FILE_SCALE_IH = 1e-9;
+        
+        UNCAL_STEP_V = -1.22e-6;
+        UNCAL_STEP_IL = 351e-12;
+        UNCAL_STEP_IH = 63.6e-9;
         
         CAL_NUM_POINTS = 201;
         CAL_STEP_V = 100e-3;
@@ -81,8 +88,8 @@ classdef rl_cal
             
             % read in file
             file = fopen(num2str(filename),'r');
-            offsets = fread(file,8,'int');
-            scales = fread(file,8,'double');
+            offsets = fread(file,rl_cal.CHANNEL_COUNT,'int');
+            scales = fread(file,rl_cal.CHANNEL_COUNT,'double');
             fclose(file);
             
             % create object            
@@ -108,106 +115,90 @@ classdef rl_cal
             i1l = i1l / rl_cal.FILE_SCALE_IL;
             i2h = i2h / rl_cal.FILE_SCALE_IH;
             i2l = i2l / rl_cal.FILE_SCALE_IL;
-
-            v_step = 80e3;                    % actual steps are about 80k
-            v_step_ideal = 100000;
-
-            il_step = 45e3;                   % actual steps are about 45k
-            il_step_ideal = 2000000;
-
-            ih_step = 11e3;                   % actual steps are about 11k
-            ih_step_ideal = 2000000;
-            ih_max = 100e6;
+            
+            v_step_uncal = rl_cal.CAL_STEP_V / abs(rl_cal.UNCAL_STEP_V);
+            il_step_uncal = rl_cal.CAL_STEP_IL / abs(rl_cal.UNCAL_STEP_IL);
+            ih_step_uncal = rl_cal.CAL_STEP_IH / abs(rl_cal.UNCAL_STEP_IH);
 
             % init
-            scales = zeros(1,8);
-            offsets = zeros(1,8);
+            scales = zeros(1,rl_cal.CHANNEL_COUNT);
+            offsets = zeros(1,rl_cal.CHANNEL_COUNT);
 
             %% Voltages
-            % indices channels vector
-            v_indices = [3,4,7,8];
 
             % ideal values
-            tmp1 = -5000000:v_step_ideal:(5000000+-v_step_ideal);
-            tmp2 = 5000000:-v_step_ideal:-5000000;
-            v_ideal = horzcat(tmp1, tmp2);
-            v_ideal2 = v_ideal*1e-6;
+            v_max = (rl_cal.CAL_NUM_POINTS-1)/4*rl_cal.CAL_STEP_V;
+            v_ideal = rl_gen_dual_sweep_values(-v_max, v_max, rl_cal.CAL_STEP_V)/rl_cal.FILE_SCALE_V;
 
-            % values
-            avg_points = zeros(4,rl_cal.CAL_NUM_POINTS);
+            % init
+            avg_points_v = zeros(4,rl_cal.CAL_NUM_POINTS);
+            residual_v = zeros(4, rl_cal.CAL_NUM_POINTS);
 
             % average and fitting
             for i=1:4
                 disp(['Voltage Channel: ', int2str(i)]);
-                avg_points(i,:) = rl_aux_average_points(v(:,i), rl_cal.CAL_NUM_POINTS, v_step, rl_cal.CAL_MIN_STABLE_SAMPLES);
-                [scales(v_indices(i)), offsets(v_indices(i)), ~ ] = rl_aux_lin_fit(avg_points(i,:),v_ideal);
-                residual(i,:) = (scales(v_indices(i))*avg_points(i,:)+offsets(v_indices(i))-v_ideal)*1e-6;
+                avg_points_v(i,:) = rl_aux_average_points(v(:,i), rl_cal.CAL_NUM_POINTS, v_step_uncal, rl_cal.CAL_MIN_STABLE_SAMPLES);
+                [scales(rl_cal.V_INDEX(i)), offsets(rl_cal.V_INDEX(i)), ~ ] = rl_aux_lin_fit(avg_points_v(i,:),v_ideal);
+                residual_v(i,:) = (scales(rl_cal.V_INDEX(i))*avg_points_v(i,:)+offsets(rl_cal.V_INDEX(i))-v_ideal)*1e-6;
             end
 
             if plotPareto ~= 0
-                rl_pareto_error(v_ideal*1e-6, residual);
+                rl_pareto_error(v_ideal*1e-6, residual_v);
                 title('Voltage Pareto Optimal Error Numbers');
             end
 
 
 
             %% Low Currents
-            % indices in all channels
-            il_indices = [2,6];
 
             % ideal values
-            tmp1 = -100000000:il_step_ideal:(100000000-il_step_ideal);
-            tmp2 = 100000000:-il_step_ideal:-100000000;
-            il_ideal = horzcat(tmp1, tmp2);
+            il_max = (rl_cal.CAL_NUM_POINTS-1)/4*rl_cal.CAL_STEP_IL;
+            il_ideal = rl_gen_dual_sweep_values(-il_max, il_max, rl_cal.CAL_STEP_IL)/rl_cal.FILE_SCALE_IL;
 
             % init
-            avg_points = zeros(2,rl_cal.CAL_NUM_POINTS);
+            avg_points_il = zeros(2,rl_cal.CAL_NUM_POINTS);
 
             % average and fitting
             disp('Current Channel 1, LOW');
-            avg_points(1,:) = rl_aux_average_points(i1l, rl_cal.CAL_NUM_POINTS, il_step, rl_cal.CAL_MIN_STABLE_SAMPLES);
-            [scales(il_indices(1)), offsets(il_indices(1)), ~ ] = rl_aux_lin_fit(avg_points(1,:),il_ideal);
+            avg_points_il(1,:) = rl_aux_average_points(i1l, rl_cal.CAL_NUM_POINTS, il_step_uncal, rl_cal.CAL_MIN_STABLE_SAMPLES);
+            [scales(rl_cal.IL_INDEX(1)), offsets(rl_cal.IL_INDEX(1)), ~ ] = rl_aux_lin_fit(avg_points_il(1,:),il_ideal);
 
             disp('Current Channel 2, LOW');
-            avg_points(2,:) = rl_aux_average_points(i2l, rl_cal.CAL_NUM_POINTS, il_step, rl_cal.CAL_MIN_STABLE_SAMPLES);
-            [scales(il_indices(2)), offsets(il_indices(2)), ~ ] = rl_aux_lin_fit(avg_points(2,:),il_ideal);
+            avg_points_il(2,:) = rl_aux_average_points(i2l, rl_cal.CAL_NUM_POINTS, il_step_uncal, rl_cal.CAL_MIN_STABLE_SAMPLES);
+            [scales(rl_cal.IL_INDEX(2)), offsets(rl_cal.IL_INDEX(2)), ~ ] = rl_aux_lin_fit(avg_points_il(2,:),il_ideal);
 
-            residual = [];
-            residual(1,:) = (scales(il_indices(1))*avg_points(1,:)+offsets(il_indices(1))-il_ideal)*1e-11;
-            residual(2,:) = (scales(il_indices(2))*avg_points(2,:)+offsets(il_indices(2))-il_ideal)*1e-11;
+            residual_il = zeros(2, rl_cal.CAL_NUM_POINTS);
+            residual_il(1,:) = (scales(rl_cal.IL_INDEX(1))*avg_points_il(1,:)+offsets(rl_cal.IL_INDEX(1))-il_ideal)*1e-11;
+            residual_il(2,:) = (scales(rl_cal.IL_INDEX(2))*avg_points_il(2,:)+offsets(rl_cal.IL_INDEX(2))-il_ideal)*1e-11;
             if plotPareto ~= 0
-                rl_pareto_error(il_ideal*1e-11, residual);
+                rl_pareto_error(il_ideal*1e-11, residual_il);
                 title('Current Low Pareto Optimal Error Numbers');
             end
 
 
             %% High Currents
 
-            % indices in all channels
-            ih_indices = [1,5];
-
             % ideal values
-            tmp1 = -ih_max:ih_step_ideal:(ih_max-ih_step_ideal);
-            tmp2 = ih_max:-ih_step_ideal:-ih_max;
-            ih_ideal = horzcat(tmp1, tmp2);
+            ih_max = (rl_cal.CAL_NUM_POINTS-1)/4*rl_cal.CAL_STEP_IH;
+            ih_ideal = rl_gen_dual_sweep_values(-ih_max, ih_max, rl_cal.CAL_STEP_IH) / rl_cal.FILE_SCALE_IH;
 
             % init
-            avg_points = zeros(2,rl_cal.CAL_NUM_POINTS);
+            avg_points_ih = zeros(2,rl_cal.CAL_NUM_POINTS);
 
             % average and fitting
             disp('Current Channel 1, HIGH');
-            avg_points(1,:) = rl_aux_average_points(i1h, rl_cal.CAL_NUM_POINTS, ih_step, rl_cal.CAL_MIN_STABLE_SAMPLES);
-            [scales(ih_indices(1)), offsets(ih_indices(1)), ~ ] = rl_aux_lin_fit(avg_points(1,:),ih_ideal);
+            avg_points_ih(1,:) = rl_aux_average_points(i1h, rl_cal.CAL_NUM_POINTS, ih_step_uncal, rl_cal.CAL_MIN_STABLE_SAMPLES);
+            [scales(rl_cal.IH_INDEX(1)), offsets(rl_cal.IH_INDEX(1)), ~ ] = rl_aux_lin_fit(avg_points_ih(1,:),ih_ideal);
 
             disp('Current Channel 2, HIGH');
-            avg_points(2,:) = rl_aux_average_points(i2h, rl_cal.CAL_NUM_POINTS, ih_step, rl_cal.CAL_MIN_STABLE_SAMPLES);
-            [scales(ih_indices(2)), offsets(ih_indices(2)), ~ ] = rl_aux_lin_fit(avg_points(2,:),ih_ideal);
+            avg_points_ih(2,:) = rl_aux_average_points(i2h, rl_cal.CAL_NUM_POINTS, ih_step_uncal, rl_cal.CAL_MIN_STABLE_SAMPLES);
+            [scales(rl_cal.IH_INDEX(2)), offsets(rl_cal.IH_INDEX(2)), ~ ] = rl_aux_lin_fit(avg_points_ih(2,:),ih_ideal);
 
-            residual = [];
-            residual(1,:) = (scales(ih_indices(1))*avg_points(1,:)+offsets(ih_indices(1))-ih_ideal)*1e-9;
-            residual(2,:) = (scales(ih_indices(2))*avg_points(2,:)+offsets(ih_indices(2))-ih_ideal)*1e-9;
+            residual_ih = zeros(2, rl_cal.CAL_NUM_POINTS);
+            residual_ih(1,:) = (scales(rl_cal.IH_INDEX(1))*avg_points_ih(1,:)+offsets(rl_cal.IH_INDEX(1))-ih_ideal)*1e-9;
+            residual_ih(2,:) = (scales(rl_cal.IH_INDEX(2))*avg_points_ih(2,:)+offsets(rl_cal.IH_INDEX(2))-ih_ideal)*1e-9;
             if plotPareto ~= 0
-                rl_pareto_error(ih_ideal*1e-9, residual);
+                rl_pareto_error(ih_ideal*1e-9, residual_ih);
                 title('Current High Pareto Optimal Error Numbers');
             end
 
@@ -217,6 +208,10 @@ classdef rl_cal
             %scales = - 1 .* scales;
             
             obj = rl_cal(offsets, scales);
+        end
+        
+        function tf = eq(h1, h2) 
+            tf = isequal(h1.offsets, h2.offsets) && isequal(h2.offsets, h2.offsets);
         end
         
     end
