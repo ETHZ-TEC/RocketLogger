a591c6bc8810ff5046bc02e4a7038315a8d5facb
mleubin <mleubin@student.ethz.ch>
2016-11-21 14:03:34 +0100

    fixed php index bug

--- code/webserver/data/rl.php
+++ code/webserver/data/rl.php
@@ -9,61 +9,61 @@
 				$parse_error = false;
 				
 				// parse and check attributes
-				if(preg_match("/^\d+$/", $_POST[sampleRate]) == 1) {
-					$command = $command . " -r " . $_POST[sampleRate];
+				if(preg_match("/^\d+$/", $_POST['sampleRate']) == 1) {
+					$command = $command . " -r " . $_POST['sampleRate'];
 				} else {
 					$parse_error = true;
 				}
-				if(preg_match("/^\d+$/", $_POST[updateRate]) == 1) {
-					$command = $command . " -u " . $_POST[updateRate];
+				if(preg_match("/^\d+$/", $_POST['updateRate']) == 1) {
+					$command = $command . " -u " . $_POST['updateRate'];
 				} else {
 					$parse_error = true;
 				}
-				if(preg_match("/^\d(,\d)*$/", $_POST[channels]) == 1) {
-					$command = $command . " -ch " . $_POST[channels];
+				if(preg_match("/^\d(,\d)*$/", $_POST['channels']) == 1) {
+					$command = $command . " -ch " . $_POST['channels'];
 				} else {
 					$parse_error = true;
 				}
-				if(preg_match("/^\d(,\d)?$/", $_POST[forceHigh]) == 1) {
-					$command = $command . " -fhr " . $_POST[forceHigh];
+				if(preg_match("/^\d(,\d)?$/", $_POST['forceHigh']) == 1) {
+					$command = $command . " -fhr " . $_POST['forceHigh'];
 				} else {
 					$parse_error = true;
 				}
-				if($_POST[ignoreCalibration] == '1') {
+				if($_POST['ignoreCalibration'] == '1') {
 					$command = $command . " -c 0";
 				} else {
 					$command = $command . " -c";
 				}
-				if($_POST[fileName] == '0') {
+				if($_POST['fileName'] == '0') {
 					$command = $command . " -f 0";
-				} else if(preg_match("/^\w+\.[A-Za-z]{3}$/", $_POST[fileName]) == 1) {
-					$command = $command . " -f /var/www/data/" . $_POST[fileName];
+				} else if(preg_match("/^\w+\.[A-Za-z]{3}$/", $_POST['fileName']) == 1) {
+					$command = $command . " -f /var/www/data/" . $_POST['fileName'];
 				} else {
 					$parse_error = true;
 				}
-				if($_POST[fileFormat] == "bin") {
+				if($_POST['fileFormat'] == "bin") {
 					$command = $command . " -format bin";
-				} else if($_POST[fileFormat] == "csv") {
+				} else if($_POST['fileFormat'] == "csv") {
 					$command = $command . " -format csv";
 				}
-				if($_POST[fileSize] == '0') {
+				if($_POST['fileSize'] == '0') {
 					$command = $command . " -size 0";
-				} else if(preg_match("/^\d+[mg]?$/", $_POST[fileSize]) == 1) {
-					$command = $command . " -size " . $_POST[fileSize];
+				} else if(preg_match("/^\d+[mg]?$/", $_POST['fileSize']) == 1) {
+					$command = $command . " -size " . $_POST['fileSize'];
 				} else {
 					$parse_error = true;
 				}
-				if($_POST[digitalInputs] == '1') {
+				if($_POST['digitalInputs'] == '1') {
 					$command = $command . " -d";
 				} else {
 					$command = $command . " -d 0";
 				}
-				if($_POST[webServer] == '1') {
+				if($_POST['webServer'] == '1') {
 					$command = $command . " -w";
 				} else {
 					$command = $command . " -w 0";
 				}
-				if($_POST[setDefault] == '1') {
+				if($_POST['setDefault'] == '1') {
 					$command = $command . " -s";
 				}
 				
