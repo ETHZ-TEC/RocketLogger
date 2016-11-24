bf21d6fadf574ea556a6d52210e961a7c4b68c44
mleubin <mleubin@student.ethz.ch>
2016-11-24 09:56:20 +0100

    New cal data files

--- code/matlab/rl_do_cal.m
+++ code/matlab/rl_do_cal.m
@@ -13,8 +13,8 @@ end
 %% read files
 rld_il1 = rld('20161122_i1l.rld');
 rld_il2 = rld('20161122_i2l.rld');
-rld_ih = rld('20161122_ih.rld');
-rld_v = rld('20161122_v.rld');
+rld_ih = rld('20161124_ih.rld');
+rld_v = rld('20161124_v.rld');
 
 %% perform fitting
 cal = rl_cal.calibrate( ...
