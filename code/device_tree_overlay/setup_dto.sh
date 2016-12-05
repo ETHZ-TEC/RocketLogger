9bd2cbff3c50177b221a5adad20d0ad115fc5702
mleubin <mleubin@student.ethz.ch>
2016-12-05 15:38:38 +0100

    Removed old dto files, renamed new one

--- code/device_tree_overlay/setup_dto.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# RocketLogger device tree overlay configuration
-
-# compile and copy device tree overlay
-dtc -O dtb -o ROCKETLOGGER-00A0.dtbo -b 0 -@ ROCKETLOGGER.dts
-cp ROCKETLOGGER-00A0.dtbo /lib/firmware
-
-# load device tree overlay at boot
-cp capemgr /etc/default/capemgr
