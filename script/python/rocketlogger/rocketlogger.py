cba5a04aad8bb057238d7da785269417acf079e5
Lukas Sigrist <lukas.sigrist@tik.ee.ethz.ch>
2016-12-22 17:10:35 +0100

    add actual data decoding to import functionality

--- script/python/rocketlogger/rocketlogger.py
+++ script/python/rocketlogger/rocketlogger.py
@@ -11,78 +11,98 @@ import numpy as np
 # import matplotlib.pyplot as plt
 
 
+_ROCKETLOGGER_FILE_MAGIC = 0x444C5225
+
+_TIMESTAMP_SECONDS_BYTES = 8
+_TIMESTAMP_NANOSECONDS_BYTES = 8
+_TIMESTAMP_BYTES = _TIMESTAMP_SECONDS_BYTES + _TIMESTAMP_NANOSECONDS_BYTES
+
+_FILE_MAGIC_BYTES = 4
+_FILE_VERSION_BYTES = 2
+_HEADER_LENGHT_BYTES = 2
+_DATA_BLOCK_SIZE_BYTES = 4
+_DATA_BLOCK_COUNT_BYTES = 4
+_SAMPLE_COUNT_BYTES = 8
+_SAMPLE_RATE_BYTES = 2
+_MAC_ADDRESS_BYTES = 6
+_COMMENT_LENGTH_BYTES = 4
+_CHANNEL_BINARY_COUNT_BYTES = 2
+_CHANNEL_ANALOG_COUNT_BYTES = 2
+
+_CHANNEL_UNIT_INDEX_BYTES = 4
+_CHANNEL_SCALE_BYTES = 4
+_CHANNEL_DATA_BYTES_BYTES = 2
+_CHANNEL_VALID_LINK_BYTES = 2
+_CHANNEL_NAME_BYTES = 16
+
+_CHANNEL_UNIT_NAMES = ['Undefined',
+                       'Voltage',
+                       'Current',
+                       'Binary',
+                       'Range Valid (binary)']
+_CHANNEL_IS_BINARY = [False, False, False, True, True]
+
+
+def _read_uint(file_handle, data_size):
+    """
+    Read an unsigned integer of defined data_size from file.
+    file_handle:    The file handle to read from at current position.
+    data_size:     The width in bytes of the integer to read.
+    return value:   The integer read and decoded.
+    """
+    return int.from_bytes(file_handle.read(data_size), byteorder='little', signed=False)
+
+
+def _read_int(file_handle, data_size):
+    """
+    Read a signed integer of defined data_size from file.
+    file_handle:    The file handle to read from at current position.
+    data_size:     The width in bytes of the integer to read.
+    return value:   The integer read and decoded.
+    """
+    return int.from_bytes(file_handle.read(data_size), byteorder='little', signed=True)
+
+
+def _read_str(file_handle, length):
+    """
+    Read a string of defined length from file.
+    file_handle:    The file handle to read from at current position.
+    data_size:     The length of the string to read.
+    return value:   The string read and decoded.
+    """
+    raw_bytes = file_handle.read(length)
+    try:
+        text = raw_bytes.split(b'\x00')[0].decode(encoding='ascii')
+    except:
+        print('ERROR: failed decoding the string: {0}'.format(raw_bytes))
+        text = 'ERROR'
+
+    return text
+
+
+def _read_timestamp(file_handle):
+    """
+    Read a timestamp from the file.
+    file_handle:    The file handle to read from at current position.
+    return value:   The read date and time as datetime object.
+    """
+    seconds = _read_int(file_handle, _TIMESTAMP_SECONDS_BYTES)
+    nanoseconds = _read_int(file_handle, _TIMESTAMP_NANOSECONDS_BYTES)
+
+    try:
+        timestamp = datetime.fromtimestamp(seconds + 1e-9*nanoseconds, timezone.utc)
+    except:
+        timestamp = datetime.min
+        print('ERROR: Timestamp conversion failed, using min. value.')
+        print('s={0} ({0:x}), ns={1} ({1:x})'.format(seconds, nanoseconds))
+
+    return timestamp
+
+
 class RocketLoggerData:
     """
     RocketLogger Data handling class
     """
-
-    ROCKETLOGGER_FILE_MAGIC = 0x444C5225
-
-    TIMESTAMP_SECONDS_BYTES = 8
-    TIMESTAMP_NANOSECONDS_BYTES = 8
-
-    FILE_MAGIC_BYTES = 4
-    FILE_VERSION_BYTES = 2
-    HEADER_LENGHT_BYTES = 2
-    DATA_BLOCK_SIZE_BYTES = 4
-    DATA_BLOCK_COUNT_BYTES = 4
-    SAMPLE_COUNT_BYTES = 8
-    SAMPLE_RATE_BYTES = 2
-    MAC_ADDRESS_BYTES = 6
-    COMMENT_LENGTH_BYTES = 4
-    CHANNEL_BINARY_COUNT_BYTES = 2
-    CHANNEL_ANALOG_COUNT_BYTES = 2
-
-    CHANNEL_UNIT_INDEX_BYTES = 4
-    CHANNEL_SCALE_BYTES = 4
-    CHANNEL_DATA_BYTES_BYTES = 2
-    CHANNEL_VALID_LINK_BYTES = 2
-    CHANNEL_NAME_BYTES = 16
-
-    CHANNEL_UNIT_NAMES = ['Undefined',
-                          'Voltage',
-                          'Current',
-                          'Binary',
-                          'Range Valid (binary)']
-
-    def read_uint(self, file_handle, data_width):
-        """
-        Read an unsigned integer of defined data_size from file.
-        file_handle:    The file handle to read from at current position.
-        data_width:     The width in bytes of the integer to read.
-        return value:   The integer read and decoded.
-        """
-        return int.from_bytes(file_handle.read(data_width), byteorder='little', signed=False)
-
-    def read_int(self, file_handle, data_width):
-        """
-        Read a signed integer of defined data_size from file.
-        file_handle:    The file handle to read from at current position.
-        data_width:     The width in bytes of the integer to read.
-        return value:   The integer read and decoded.
-        """
-        return int.from_bytes(file_handle.read(data_width), byteorder='little', signed=True)
-
-    def read_str(self, file_handle, length):
-        """
-        Read a string of defined length from file.
-        file_handle:    The file handle to read from at current position.
-        data_width:     The length of the string to read.
-        return value:   The string read and decoded.
-        """
-        return file_handle.read(length).decode(encoding='ascii').rstrip('\x00')
-
-    def read_timestamp(self, file_handle):
-        """
-        Read a timestamp from the file.
-        file_handle:    The file handle to read from at current position.
-        return value:   The read date and time as datetime object.
-        """
-        seconds = self.read_uint(file_handle, self.TIMESTAMP_SECONDS_BYTES)
-        nanoseconds = self.read_uint(file_handle, self.TIMESTAMP_NANOSECONDS_BYTES)
-        timestamp = datetime.fromtimestamp(seconds, timezone.utc)
-        timestamp = timestamp + timedelta(milliseconds=nanoseconds/1000)
-        return timestamp
 
     def __init__(self, filename, decimation_factor=1):
         """
@@ -95,7 +115,7 @@ class RocketLoggerData:
         else:
             self._channels = []
 
-    def read_file_header(self, file_handle):
+    def _read_file_header(self, file_handle):
         """
         Read a RocketLogger data file's header, including comment and channels.
         file_handle:    The file handle to read from, with pointer positioned at file start
@@ -103,91 +123,128 @@ class RocketLoggerData:
         """
         header = dict()
 
-        header['file_magic'] = self.read_uint(file_handle, self.FILE_MAGIC_BYTES)
-        header['file_version'] = self.read_uint(file_handle, self.FILE_VERSION_BYTES)
+        header['file_magic'] = _read_uint(file_handle, _FILE_MAGIC_BYTES)
+        header['file_version'] = _read_uint(file_handle, _FILE_VERSION_BYTES)
 
         # file consistency check
-        if header['file_magic'] != self.ROCKETLOGGER_FILE_MAGIC:
+        if header['file_magic'] != _ROCKETLOGGER_FILE_MAGIC:
             print('Invalid RocketLogger data file, file magic missmatch {0:x}.'
                   .format(header['file_magic']))
 
         # read static header fields
-        header['header_length'] = self.read_uint(file_handle, self.HEADER_LENGHT_BYTES)
-        header['data_block_size'] = self.read_uint(file_handle, self.DATA_BLOCK_SIZE_BYTES)
-        header['data_block_count'] = self.read_uint(file_handle, self.DATA_BLOCK_COUNT_BYTES)
-        header['sample_count'] = self.read_uint(file_handle, self.SAMPLE_COUNT_BYTES)
-        header['sample_rate'] = self.read_uint(file_handle, self.SAMPLE_RATE_BYTES)
-        header['mac_address'] = file_handle.read(self.MAC_ADDRESS_BYTES)
-        header['start_time'] = self.read_timestamp(file_handle)
-        header['comment_length'] = self.read_uint(file_handle, self.COMMENT_LENGTH_BYTES)
-        header['channel_binary_count'] = self.read_uint(file_handle,
-                                                        self.CHANNEL_BINARY_COUNT_BYTES)
-        header['channel_analog_count'] = self.read_uint(file_handle,
-                                                        self.CHANNEL_ANALOG_COUNT_BYTES)
+        header['header_length'] = _read_uint(file_handle, _HEADER_LENGHT_BYTES)
+        header['data_block_size'] = _read_uint(file_handle, _DATA_BLOCK_SIZE_BYTES)
+        header['data_block_count'] = _read_uint(file_handle, _DATA_BLOCK_COUNT_BYTES)
+        header['sample_count'] = _read_uint(file_handle, _SAMPLE_COUNT_BYTES)
+        header['sample_rate'] = _read_uint(file_handle, _SAMPLE_RATE_BYTES)
+        header['mac_address'] = bytearray(file_handle.read(_MAC_ADDRESS_BYTES))
+        header['start_time'] = _read_timestamp(file_handle)
+        header['comment_length'] = _read_uint(file_handle, _COMMENT_LENGTH_BYTES)
+        header['channel_binary_count'] = _read_uint(file_handle, _CHANNEL_BINARY_COUNT_BYTES)
+        header['channel_analog_count'] = _read_uint(file_handle, _CHANNEL_ANALOG_COUNT_BYTES)
 
         # header consistency checks
         if header['comment_length'] % 4 > 0:
-            print('Missaligned comment length {0}.'.format(header['comment_length']))
-            # header['comment_length'] = 4 * ceil(header['comment_length'] / 4)
+            print('Comment length unaligned {0}.'.format(header['comment_length']))
 
         if header['sample_count'] != header['data_block_count'] * header['data_block_size']:
             print('Inconsistency in number of samples taken!')
             return dict()
 
         # read comment field
-        header['comment'] = self.read_str(file_handle, header['comment_length'])
+        header['comment'] = _read_str(file_handle, header['comment_length'])
 
         # read channel headers
         header['channels'] = list()
         for ch in range(header['channel_binary_count'] + header['channel_analog_count']):
             channel = dict()
-            channel['unit_index'] = self.read_int(file_handle, self.CHANNEL_UNIT_INDEX_BYTES)
-            channel['scale'] = self.read_int(file_handle, self.CHANNEL_SCALE_BYTES)
-            channel['data_width'] = self.read_uint(file_handle, self.CHANNEL_DATA_BYTES_BYTES)
-            channel['valid_link'] = self.read_uint(file_handle, self.CHANNEL_VALID_LINK_BYTES)
-            channel['name'] = self.read_str(file_handle, self.CHANNEL_NAME_BYTES)
+            channel['unit_index'] = _read_int(file_handle, _CHANNEL_UNIT_INDEX_BYTES)
+            channel['scale'] = _read_int(file_handle, _CHANNEL_SCALE_BYTES)
+            channel['data_size'] = _read_uint(file_handle, _CHANNEL_DATA_BYTES_BYTES)
+            channel['valid_link'] = _read_uint(file_handle, _CHANNEL_VALID_LINK_BYTES)
+            channel['name'] = _read_str(file_handle, _CHANNEL_NAME_BYTES)
 
             try:
-                channel['unit'] = self.CHANNEL_UNIT_NAMES[channel['unit_index']]
+                channel['unit'] = _CHANNEL_UNIT_NAMES[channel['unit_index']]
             except:
                 channel['unit_index'] = 0
-                channel['unit'] = self.CHANNEL_UNIT_NAMES[0]
+                channel['unit'] = _CHANNEL_UNIT_NAMES[0]
 
             header['channels'].append(channel)
 
         return header
 
-    def read_file_data_block(self, file_handle, file_header):
+    def _read_file_data(self, file_handle, file_header, decimation=1):
         """
         Read a data block at the current position in the RocketLogger data file
         file_handle:    The file handle to read from, with pointer positioned at begining of block
         file_header:    The file's header with the data alignment details.
-        return value:   Tuple of the block timestamp and data array containing the read data block.
+        return value:   Tuple of the realtime timestamp, monotonic timestamp and
+                        the actual data array containing the read sample data.
         """
 
-        timestamp = datetime.min
-        data = np.zeros((file_header['data_block_size'], file_header['channel_binary_count'] +
-                         file_header['channel_analog_count']))
+        # generate data type to read from header info
+        total_bin_bytes = 4 * ceil(file_header['channel_binary_count'] / 32)
 
-        # decode block timestamp
-        # TODO: convert to date time
-        timestamp = self.read_timestamp(file_handle)
+        type_names = ['bin']
+        type_formats = ['<u{0}'.format(total_bin_bytes)]
+        for channel in file_header['channels']:
+            if not _CHANNEL_IS_BINARY[channel['unit_index']]:
+                type_names.append(channel['name'])
+                type_formats.append('<i{0}'.format(channel['data_size']))
 
-        # decode block data
-        total_channel_count = ceil(file_header['channel_binary_count'] / 32) + \
-            file_header['channel_analog_count']
-        data_read_count = file_header['data_block_size'] * total_channel_count
+        # read raw data from file
+        data_type = np.dtype({'names': type_names, 'formats': type_formats})
 
-        # TODO: read data
-        data = np.fromfile(file_handle, dtype=np.uint32, count=data_read_count)
+        # allocate the data
+        timestamps_realtime = list()
+        timestamps_monotonic = list()
+        data = np.empty((file_header['data_block_count'] * file_header['data_block_size'],
+                        file_header['channel_binary_count'] + file_header['channel_analog_count']))
 
-        # TODO: split and decode channels
-        pass
+        # iterate over all data blocks
+        for block_index in range(file_header['data_block_count']):
+            block_offset_bytes = file_header['header_length'] + block_index * \
+                (2 * _TIMESTAMP_BYTES + file_header['data_block_size'] * data_type.itemsize)
+            data_index_start = block_index * file_header['data_block_size']
+            data_index_end = data_index_start + file_header['data_block_size']
 
-        # TODO: decimation
-        pass
+            # read block timestamp
+            file_handle.seek(block_offset_bytes)
+            timestamps_realtime.append(_read_timestamp(file_handle))
+            timestamps_monotonic.append(_read_timestamp(file_handle))
 
-        return timestamp, data
+            # access file data memory mapped
+            # data_raw = np.fromfile(file_handle, dtype=data_type,
+            #                        count=file_header['data_block_size'])
+            file_data = np.memmap(file_handle, offset=block_offset_bytes +
+                                  2 * _TIMESTAMP_BYTES, mode='r', dtype=data_type,
+                                  shape=file_header['data_block_size'])
+
+            # extract binary channels
+            # binary_data_bool = np.unpackbits(file_data['bin']).reshape(
+            #     (file_header['data_block_size'], 8 * total_bin_bytes))
+
+            # TODO: map to data type used ot store binary value
+            for binary_channel_index in range(file_header['channel_binary_count']):
+                channel_index = binary_channel_index
+                data[data_index_start:data_index_end, channel_index] = np.array(
+                    2**binary_channel_index & file_data['bin'], dtype=data.dtype)
+
+            for analog_channel_index in range(file_header['channel_analog_count']):
+                channel_index = file_header['channel_binary_count'] + analog_channel_index
+                channel_name = type_names[analog_channel_index + 1]
+                data[data_index_start:data_index_end, channel_index] = \
+                    10**file_header['channels'][channel_index]['scale'] * file_data[channel_name]
+
+            # extract analog channels
+            pass
+
+            # decimate block data
+            # TODO: decimantion
+            pass
+
+        return timestamps_realtime, timestamps_monotonic, data
 
     def read_file(self, filename, decimation_factor=1):
         """
@@ -203,7 +260,7 @@ class RocketLoggerData:
         while isfile(file_name):
             with open(file_name, 'rb') as file_handle:
                 # read file header
-                header = self.read_file_header(file_handle)
+                header = self._read_file_header(file_handle)
 
                 # consistency check: file stream position matches header size
                 if 'header_length' not in header:
@@ -216,7 +273,6 @@ class RocketLoggerData:
                           .format(stream_position, header['header_length']))
                     return
 
-                # decimation
                 if (header['data_block_size'] % decimation_factor) > 0:
                     print('ERROR: Decimation factor needs to be divider of the buffer size.')
                     return
@@ -234,8 +290,19 @@ class RocketLoggerData:
                     self._header['data_block_count'] = self._header['data_block_count'] + \
                         header['data_block_count']
 
-                # channels: read data
-                block_timestamp, block_data = self.read_file_data_block(file_handle, header)
+                # channels: read actual sampled data
+                timestamps_realtime, timestamps_monotonic, data = \
+                    self._read_file_data(file_handle, header)
+
+                # create on first file, append on following
+                if file_number > 0:
+                    self._timstamps_realtime.append(timestamps_realtime)
+                    self._timstamps_monotonic.append(timestamps_monotonic)
+                    self._data = np.vstack(self._data, data)
+                else:
+                    self._timstamps_realtime = timestamps_realtime
+                    self._timstamps_monotonic = timestamps_monotonic
+                    self._data = data
 
             file_name = '{0}-p{1}.{2}'.format(file_basename, file_number, file_extension)
             file_number = file_number + 1
