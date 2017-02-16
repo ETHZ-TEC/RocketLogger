e3283906593bcd358bfb31ea86544c6ad32dfd14
Lukas Sigrist <lukas.sigrist@tik.ee.ethz.ch>
2017-02-16 17:04:32 +0100

    add prefix to calibration seed rng by default to get deterministic calibration result, closes #92

--- script/matlab/rl_do_cal.m
+++ script/matlab/rl_do_cal.m
@@ -1,4 +1,4 @@
-function [cal] = rl_do_cal (create_plots)
+function cal = rl_do_cal(create_plots)
 %RL_DO_CAL Perform a calibration of the RocketLogger using predefined files
 %   Parameters:
 %      - create_plots:     Plot residuals and pareto optimal error figures
@@ -11,10 +11,15 @@ if ~exist('create_plots', 'var')
 end
 
 %% read files
-rld_il1 = rld('20161122_i1l.rld');
-rld_il2 = rld('20161122_i2l.rld');
-rld_ih = rld('20161124_ih.rld');
-rld_v = rld('20161125_v.rld');
+file_prefix = '20170214_';
+rld_il1 = rld(sprintf('%scalibration_i1l.rld', file_prefix));
+rld_il2 = rld(sprintf('%scalibration_i2l.rld', file_prefix));
+rld_ih = rld(sprintf('%scalibration_ih.rld', file_prefix));
+rld_v = rld(sprintf('%scalibration_v.rld', file_prefix));
+
+%% seed random number generator
+% optional, uncomment for determistic selection among Parto calibration points
+rng(0);
 
 %% perform fitting
 cal = rl_cal.calibrate( ...
@@ -30,5 +35,9 @@ cal.fix_signs();
 
 %% write output file
 cal.write_file();
+
+% copy file with prefix
+copyfile('calibration.dat', sprintf('%scalibration.dat', file_prefix), 'f');
+copyfile('calibration.log', sprintf('%scalibration.log', file_prefix), 'f');
 
 end
