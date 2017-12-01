e91ccbfbbd14b1c1da745e2560833c70f99f7c34
Lukas Sigrist <lukas.sigrist@tik.ee.ethz.ch>
2017-12-01 15:59:07 +0100

    make setup scripts ready for updating installation

--- software/debian/get_image.sh
+++ software/debian/get_image.sh
@@ -9,11 +9,11 @@ IMAGE_FILE="bone-debian-7.11-console-armhf-2016-06-15-2gb.img.xz"
 IMAGE_SHA256="cfceb64083cf63ed49ad75c3b5f5665cef65eaa67c86420a2c4d27bddc22d1ee"
 
 # download image
-wget --show-progress "$URL_DIRECTORY$IMAGE_FILE"
+wget --progress=bar "$URL_DIRECTORY$IMAGE_FILE"
 
 # check downloaded file hash
-SHA=`sha256sum "$IMAGE_FILE"`
-if [[ "$SHA" == "$IMAGE_SHA256  $IMAGE_FILE" ]]; then
+SHA=`sha256sum "$IMAGE_FILE" | awk '{print $1}'`
+if [[ "$SHA" == "$IMAGE_SHA256" ]]; then
   echo "SHA256 hash checked successfully."
   exit 0
 else
--- software/webserver/setup_web.sh
+++ software/webserver/setup_web.sh
@@ -4,8 +4,12 @@
 # Copyright (c) 2016-2017, ETH Zurich, Computer Engineering Group
 #
 
-# disable apache server
-rm -f /etc/init.d/apache2
+WEB_ROOT="/var/www"
+WEB_SOURCE=`pwd`"/data"
+
+# disable apache web server
+systemctl disable apache2
+#rm -f /etc/init.d/apache2
 
 # disable bonescript stuff
 systemctl disable bonescript.service
@@ -16,23 +20,32 @@ systemctl disable bonescript-autorun.service
 cp -f lighttpd.conf /etc/lighttpd/lighttpd.conf
 
 # copy webserver data
-rm -rf /var/www/*
-cp -rf data/* /var/www/
-mkdir -p /var/www/data /var/www/log
+mkdir -p ${WEB_ROOT}
+rsync -aP ${WEB_SOURCE}/ ${WEB_ROOT}/
+
+# create data and log dirs
+mkdir -p ${WEB_ROOT}/data ${WEB_ROOT}/log
+#rm -rf /var/www/*
+#cp -rf data/* /var/www/
+#mkdir -p /var/www/data /var/www/log
 
 # download and copy dependencies (bootstrap, jquery, flot)
-mkdir -p /var/www/js/vendor/
+mkdir -p ${WEB_ROOT}/css/ ${WEB_ROOT}/js/vendor/
 
 wget -N https://github.com/twbs/bootstrap/releases/download/v3.3.7/bootstrap-3.3.7-dist.zip
+wget -N https://code.jquery.com/jquery-3.1.1.min.js
+wget -N http://www.flotcharts.org/downloads/flot-0.8.3.zip
+
 unzip -o bootstrap-3.3.7-dist.zip
 cp -rf bootstrap-3.3.7-dist/fonts /var/www/
 cp -f bootstrap-3.3.7-dist/css/*.min.css /var/www/css/
 cp -f bootstrap-3.3.7-dist/js/*.min.js /var/www/js/vendor/
+rm -r bootstrap-*-dist
 
-wget -N https://code.jquery.com/jquery-3.1.1.min.js
 cp -f jquery-3.1.1.min.js /var/www/js/vendor/
+rm jquery-3.1.1.min.js
 
-wget -N http://www.flotcharts.org/downloads/flot-0.8.3.zip
 unzip -o flot-0.8.3.zip
 cp -f flot/jquery.flot*.min.js /var/www/js/vendor/
+rm -r flot
 
