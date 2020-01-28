0ec46cde0e1136ad6ce946510b4115ecd4109685
Lukas Sigrist <lukas.sigrist@tik.ee.ethz.ch>
2020-01-28 16:58:50 +0100

    update install script and dependencies

--- software/webserver/install.sh
+++ software/webserver/install.sh
@@ -30,15 +30,17 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 # 
 
-WEB_ROOT="/var/www/rocketlogger"
+WEB_ROOT="$HOME/public_html"
 WEB_SOURCE=`pwd`"/data"
-WEB_AUTH_FILE=/home/rocketlogger/.htpasswd
+WEB_AUTH_FILE="$HOME/.htpasswd"
 
-LIB_DIR='./lib'
+LIB_DIR=`pwd`"/lib"
+
 BOOTSTRAP_VERSION=4.4.1
 POPPER_VERSION=1.16.0
 JQUERY_VERSION=3.4.1
 FLOT_VERSION=4.1.1
+
 
 ## package install
 echo "> Installing web interface dependencies"
@@ -65,37 +67,44 @@ sudo cp -f lighttpd.conf /etc/lighttpd/lighttpd.conf
 echo "> Deploying RocketLogger web interface"
 
 # copy webserver data
-sudo mkdir -p ${WEB_ROOT}
-sudo rm -f ${WEB_ROOT}/index.html
-sudo rsync -aP ${WEB_SOURCE}/ ${WEB_ROOT}/
+mkdir -p "${WEB_ROOT}"
+rm -f "${WEB_ROOT}/index.html"
+rsync -aP "${WEB_SOURCE}/" "${WEB_ROOT}/"
 
 # create default data and log dirs
-sudo mkdir -p ${WEB_ROOT}/data ${WEB_ROOT}/log
+# mkdir -p "${WEB_ROOT}/data" "${WEB_ROOT}/log"
 
 # download dependencies (bootstrap, popper, jquery, flot)
-sudo mkdir -p ${WEB_ROOT}/css/ ${WEB_ROOT}/js/vendor/
+mkdir -p "${WEB_ROOT}/css" "${WEB_ROOT}/js/vendor"
 
-wget --no-clobber --directory-prefix=${LIB_DIR} https://github.com/twbs/bootstrap/releases/download/v${BOOTSTRAP_VERSION}/bootstrap-${BOOTSTRAP_VERSION}-dist.zip
-wget --no-clobber --directory-prefix=${LIB_DIR} https://code.jquery.com/jquery-${JQUERY_VERSION}.min.js
-wget --no-clobber --directory-prefix=${LIB_DIR} https://unpkg.com/popper.js@${POPPER_VERSION}/dist/umd/popper.min.js
-wget --no-clobber --directory-prefix=${LIB_DIR} https://raw.githubusercontent.com/flot/flot/v${FLOT_VERSION}/source/jquery.flot.js
-wget --no-clobber --directory-prefix=${LIB_DIR} https://raw.githubusercontent.com/flot/flot/v${FLOT_VERSION}/source/jquery.flot.time.js
+wget --no-clobber --directory-prefix="${LIB_DIR}" https://github.com/twbs/bootstrap/releases/download/v${BOOTSTRAP_VERSION}/bootstrap-${BOOTSTRAP_VERSION}-dist.zip
+wget --no-clobber --directory-prefix="${LIB_DIR}" https://code.jquery.com/jquery-${JQUERY_VERSION}.min.js
+wget --no-clobber --directory-prefix="${LIB_DIR}" https://unpkg.com/popper.js@${POPPER_VERSION}/dist/umd/popper.min.js
+wget --no-clobber --directory-prefix="${LIB_DIR}" "https://registry.npmjs.org/flot/-/flot-${FLOT_VERSION}.tgz"
 
 # copy relevant file and clean up
-unzip -o bootstrap-${BOOTSTRAP_VERSION}-dist.zip
-sudo cp -f bootstrap-${BOOTSTRAP_VERSION}-dist/css/bootstrap.min.css ${WEB_ROOT}/css/bootstrap-${BOOTSTRAP_VERSION}.min.css
-sudo cp -f bootstrap-${BOOTSTRAP_VERSION}-dist/js/bootstrap.min.js ${WEB_ROOT}/js/vendor/bootstrap-${BOOTSTRAP_VERSION}.min.js
-rm -r bootstrap-${BOOTSTRAP_VERSION}-dist
-rm bootstrap-${BOOTSTRAP_VERSION}-dist.zip
+unzip -d "${LIB_DIR}" -o "${LIB_DIR}/bootstrap-${BOOTSTRAP_VERSION}-dist.zip"
+cp -f "${LIB_DIR}/bootstrap-${BOOTSTRAP_VERSION}-dist/css/bootstrap.min.css" "${WEB_ROOT}/css/bootstrap-${BOOTSTRAP_VERSION}.min.css"
+cp -f "${LIB_DIR}/bootstrap-${BOOTSTRAP_VERSION}-dist/js/bootstrap.min.js" "${WEB_ROOT}/js/vendor/bootstrap-${BOOTSTRAP_VERSION}.min.js"
+rm -r "${LIB_DIR}/bootstrap-${BOOTSTRAP_VERSION}-dist"
+rm "${LIB_DIR}/bootstrap-${BOOTSTRAP_VERSION}-dist.zip"
 
-sudo cp -f popper.min.js ${WEB_ROOT}/js/vendor/popper-${POPPER_VERSION}.min.js
-rm popper.min.js
+cp -f "${LIB_DIR}/popper.min.js" "${WEB_ROOT}/js/vendor/popper-${POPPER_VERSION}.min.js"
+rm "${LIB_DIR}/popper.min.js"
 
-sudo cp -f jquery-${JQUERY_VERSION}.min.js ${WEB_ROOT}/js/vendor/jquery-${JQUERY_VERSION}.min.js
-rm jquery-${JQUERY_VERSION}.min.js
+cp -f "${LIB_DIR}/jquery-${JQUERY_VERSION}.min.js" "${WEB_ROOT}/js/vendor/jquery-${JQUERY_VERSION}.min.js"
+rm "${LIB_DIR}/jquery-${JQUERY_VERSION}.min.js"
 
-sudo cp -f jquery.flot*.js ${WEB_ROOT}/js/vendor/
-rm jquery.flot*.js
+tar --extract --to-stdout --file "${LIB_DIR}/flot-${FLOT_VERSION}.tgz" "package/dist/es5/jquery.flot.js" > "${WEB_ROOT}/js/vendor/jquery.flot-${FLOT_VERSION}.min.js"
+rm "${LIB_DIR}/flot-${FLOT_VERSION}.tgz"
+
+# # downlaod and copy flot source files
+# FLOT_FILES=("jquery.canvaswrapper.js" "jquery.colorhelpers.js" "jquery.flot.js" "jquery.flot.saturated.js" "jquery.flot.browser.js" "jquery.flot.drawSeries.js" "jquery.flot.uiConstants.js" "jquery.flot.time.js" wget --no-clobber --directory-prefix="${LIB_DIR}" https://raw.githubusercontent.com/flot/flot/v${FLOT_VERSION}/source/jquery.canvaswrapper.js
+# for file in ${FLOT_FILES[*]}; do
+#   wget --no-clobber --directory-prefix="${LIB_DIR}" "https://raw.githubusercontent.com/flot/flot/v${FLOT_VERSION}/source/${file}"
+#   cp -f "${LIB_DIR}/${file}" "${WEB_ROOT}/js/vendor/${file/./-${FLOT_VERSION}/}"
+#   rm "${LIB_DIR}/${file}"
+# done
 
 # create web interface login if not existing
 if [ -f $WEB_AUTH_FILE ]; then
