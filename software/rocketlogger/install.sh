b742194948ea5d396e7ab885805ff32953e9963a
Lukas Sigrist <lsigrist@mailbox.org>
2021-01-31 19:20:29 +0100

    specify software installation

---
+++ software/rocketlogger/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Post install system configuration for RocketLogger software
+
+# source dir
+SRC=..
+
+# systemd service
+SERVICE_NAME="rocketlogger.service"
+
+# device tree configuration file to patch
+UENV_PATCH="${SRC}/config/uEnv.txt.patch"
+UENV_TARGET="/boot/uEnv.txt"
+
+
+## enable and start systemd service
+systemctl enable "${SERVICE_NAME}"
+systemctl restart "${SERVICE_NAME}"
+
+## patch device tree overlay configuration, with backup
+patch --forward --backup --input="${UENV_PATCH}" "${UENV_TARGET}"
