bc7896cc8cfabff9d030c579973cc2a38f4c96ce
Lukas Sigrist <lsigrist@mailbox.org>
2022-12-27 15:55:17 +0100

    make TypedArray enqueue helpers class methods

--- software/node_server/static/js/rl.data.js
+++ software/node_server/static/js/rl.data.js
@@ -527,6 +527,9 @@ function data_add(message, cache_data = false) {
 
 class AggregatingBuffer {
     constructor(TypedArrayT, size, levels, aggregation_factor, initial_value = 0) {
+        if (!TypedArrayT.hasOwnProperty('BYTES_PER_ELEMENT')) {
+            throw TypeError('supporting TypedArray types only');
+        }
         this._size = size;
         this._levels = levels;
         this._aggregation_factor = aggregation_factor;
@@ -539,15 +542,29 @@ class AggregatingBuffer {
         // aggregate data about to be dequeued to next lower buffer
         for (let i = 1; i < this._levels; i++) {
             const aggregate_count = data.length / (this._aggregation_factor ** (this._levels - i));
-            typedarray_enqueue_aggregate(this._dataLevel[i], this._dataLevel[i - 1], aggregate_count, this._aggregation_factor);
+            this.constructor._enqueue_aggregate(this._dataLevel[i], this._dataLevel[i - 1], aggregate_count, this._aggregation_factor);
         }
 
         // enqueue new data
-        typedarray_enqueue(data, this._dataLevel[this._levels - 1]);
+        this.constructor._enqueue(data, this._dataLevel[this._levels - 1]);
     }
 
     getView() {
         return this._data;
+    }
+
+    // enqueue typed array data at end of typed array buffer
+    static _enqueue(buffer_in, buffer_out) {
+        buffer_out.set(buffer_out.subarray(buffer_in.length));
+        buffer_out.set(buffer_in, buffer_out.length - buffer_in.length);
+    }
+
+    // enqueue aggregates of a typed array at end of typed array buffer
+    static _enqueue_aggregate(buffer_in, buffer_out, count, aggregation_factor) {
+        buffer_out.set(buffer_out.subarray(count));
+        for (let i = 0; i < count; i++) {
+            buffer_out[buffer_out.length - count + i] = buffer_in[i * aggregation_factor];
+        }
     }
 }
 
@@ -585,20 +602,5 @@ class AggregatingDataStore extends AggregatingBuffer {
 
     _get_start_index() {
         return this._start_index = this._data.findLastIndex(isNaN, this._start_index) + 1;
-    }
-}
-
-
-// enqueue typed array data at end of typed array buffer
-function typedarray_enqueue(buffer_in, buffer_out) {
-    buffer_out.set(buffer_out.subarray(buffer_in.length));
-    buffer_out.set(buffer_in, buffer_out.length - buffer_in.length);
-}
-
-// enqueue aggregates of a typed array at end of typed array buffer
-function typedarray_enqueue_aggregate(buffer_in, buffer_out, count, aggregation_factor) {
-    buffer_out.set(buffer_out.subarray(count));
-    for (let i = 0; i < count; i++) {
-        buffer_out[buffer_out.length - count + i] = buffer_in[i * aggregation_factor];
     }
 }
