6497346b6c7e89b96d1ca67586a4c071e906952f
Lukas Sigrist <lsigrist@mailbox.org>
2023-05-13 13:34:58 +0200

    bump version number to 2.1.1

--- script/python/docs/conf.py
+++ script/python/docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, ETH Zurich, Computer Engineering Group'
 author = 'ETH Zurich, Computer Engineering Group'
 
 # The full version, including alpha/beta/rc tags
-release = '2.1.0'
+release = '2.1.1'
 
 
 # -- General configuration ---------------------------------------------------
