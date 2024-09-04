c1e79f48aa111af99ee2afe2ede3496e9a559a56
Lukas Sigrist <lsigrist@mailbox.org>
2024-09-04 21:43:14 +0200

    Support Python 3.12

--- script/python/setup.py
+++ script/python/setup.py
@@ -58,6 +58,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3 :: Only",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
